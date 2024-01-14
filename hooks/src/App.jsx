573f09dd18cccc5062a7066cfc36eef9857d7f44
shriyans jindal <shriyansjindal40517@gmail.com>
2024-01-14 13:36:09 +0530

    done

---
+++ hooks/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Usestate from './Components/Usestate'
+import Useobject from './Components/Useobject'
+import Usefunction from './Components/Usefunction'
+import Useeffect from './Components/Useeffect'
+function App() {
+
+
+  return (
+    <>
+     <Usestate/>
+     <Useobject/>
+     <Usefunction/>
+     <Useeffect/>
+    </>
+  )
+}
+
+export default App
